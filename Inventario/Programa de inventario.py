62b32d5d91c7c8be495029141021bef74a89e6de
jopamu85 <37051526+jopamu85@users.noreply.github.com>
2019-11-24 14:59:28 -0600

    Commit inicial
    Commit inicial de Inventario

---
+++ Inventario/Programa de inventario.py
@@ -0,0 +1,61 @@
+
+def menu():
+    bandera = False
+    num = 0
+    while not bandera:
+        try:
+            num= int(input("Introduzca la opcion seleccionada "))
+            bandera = True
+        except ValueError:
+            print("Valor ingresado invalido, ingrese un digito ")
+    return num
+def ingresarCant(mensaje):
+    dato = int(input(mensaje))
+    return dato
+def ingresarNombre(mensaje):
+    nombre = input(mensaje)
+    return nombre
+def multiplicacion(var1,var2):
+    return var1 * var2
+
+salir = False
+while not salir:
+    print("Bienvenido(a) al sistema de inventario")
+    print("1. Ingresar inventario")
+    print("2. Sacar de inventario")
+    print("3. Editar inventario")
+    print("4. Listado de inventario/ Busqueda")
+    print("5. Salir")
+
+    print("Elija una opcion ")
+    opcion= menu()
+    if opcion == 1:
+        cantprod = ingresarCant("Ingrese la cantidad de productos a procesar ")
+        cont = 1
+        producto = []
+        cantidad = []
+        precio = []
+        total = []
+        while cantprod >= cont:
+            nomprod = ingresarNombre("Ingrese el nombre del producto ")
+            producto.append(nomprod)
+            var1 = ingresarCant("Ingrese la cantidad de productos a ingresar ")
+            cantidad.append(var1)
+            var2 = ingresarCant("Ingrese el precio unitario ")
+            precio.append(var2)
+            totalprod = multiplicacion(var1, var2)
+            total.append(totalprod)
+            cont += 1
+        print(producto, cantidad, precio, total)
+
+    elif opcion == 2:
+        print("Opcion 2")
+    elif opcion == 3:
+        print("Opcion 3")
+    elif opcion == 4:
+        print("Opcion 4")
+    elif opcion == 5:
+        salir= True
+    else:
+        print("Introduzca un valor entre 1 y 5")
+print("Fin")
